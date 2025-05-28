09407e7c7b0acb374735940365b6b6235c2ad71c
Reza <rparsa75@gmail.com>
2025-05-28 21:13:33 +0330

    ADD: attack animation (press space)

---
+++ Assets/Scripts/Playercombat.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Playercombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+  
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+
+
+
+
+
+}
+
